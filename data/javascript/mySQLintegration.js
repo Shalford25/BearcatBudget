a8f6630571f014f654dd52405098f3d45fe07915
Shalford25 <spencer.halford@lander.edu>
2025-03-12 13:50:01 -0400

    SQL function beginnings

--- data/javascript/mySQLintegration.js
+++ data/javascript/mySQLintegration.js
@@ -1,5 +1,4 @@
-import {useState, useContext} from "react";// Step 1
-function connectmySQL(){
+var currDB = "mydb";
 var mysql = require('mysql');
 var con = mysql.createConnection({
    host: "localhost",
@@ -7,7 +6,35 @@ var con = mysql.createConnection({
    password: "mypassword",
    database: "mydb"
 });
+var currentTable = "";
+function currTable(tableName){
+   currentTable = tableName;
+   console.log("Current Table: "+currentTable);
+}
+function addEntry(entryData){
+   if (currentTable === "") {
+      console.log("No table selected.");
+      return;
+   }
+   
+   var columns = Object.keys(entryData).join(", ");
+   var values = Object.values(entryData).map(value => `'${value}'`).join(", ");
+   var sql = `INSERT INTO ${currentTable} (${columns}) VALUES (${values})`;
 
+   con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+   });
+}
+function removeEntry(){
+
+}
+function updateEntry(){
+
+}
+function tableCompile(){
+   
+}
 var qry ="UPDATE employee SET salary=salary+500;";
 con.connect(function (err) {
    if (err) throw err;
@@ -17,4 +44,3 @@ con.connect(function (err) {
       console.log("Records updated successfully");
    });
 });
-}
