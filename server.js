203052d5f44d3f8cad97d5a8f336ec1c8a62a969
Shalford25 <spencer.halford@lander.edu>
2025-03-21 18:48:48 -0400

    backend debug for preflight check

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.use((req, res, next) => {
     next();
 });
 
+// Log incoming requests
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
 // Create a MySQL connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
